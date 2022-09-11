da647e8bb8a2a9b9d870e1e4eb0ef836cf71d588
David Chi <davidchi@tamu.edu>
2022-09-11 16:02:26 -0500

    Made Navbar better.

--- src/components/Base/index.js
+++ src/components/Base/index.js
@@ -8,15 +8,18 @@ const BaseNavbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="base__navbar">
-      <img alt="CPDuels logo" className="logo" src={logo} onClick={() => navigate("/")} />
-      <div className="base__navbar__links">
-        <ul>
-          <li><PageLink name="Play" destination="/play"/></li>
-          <li><PageLink name="Tutorial" destination="/tutorial"/></li>
-          <li><PageLink name="News" destination="/news"/></li>
-          <li><PageLink name="Contact" destination="/contact"/></li>
-        </ul>
+    <nav className="base__navbar__container">
+      <div className="base__navbar">
+        <img alt="CPDuels logo" className="logo" src={logo} onClick={() => navigate("/")} />
+        <div className="base__navbar__links">
+          <ul>
+            <li><PageLink name="Play" destination="/play"/></li>
+            <li><PageLink name="Tutorial" destination="/tutorial"/></li>
+            <li><PageLink name="News" destination="/news"/></li>
+            <li><PageLink name="Contact" destination="/contact"/></li>
+          </ul>
+        </div>
+        <button className="base__login__button rounded">Login</button>
       </div>
     </nav>
   )
@@ -24,7 +27,7 @@ const BaseNavbar = () => {
 
 const BaseContainer = ({ content }) => {
   return (
-    <div className="base__container">
+    <div className="base__content__container">
       {content}
     </div>
   )
