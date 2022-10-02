a3eef7db20521e81b644d8611b3d03633f0c550e
JelBell1324 <jeffthegreat66@gmail.com>
2022-10-02 04:38:45 -0500

    cors

--- src/config/origins.js
+++ src/config/origins.js
@@ -1,4 +1,4 @@
 import DEBUG from "./debug";
-const backendOrigin = DEBUG ? 'http://localhost:8080' : 'wss://cpduels-backend.onrender.com';
+const backendOrigin = DEBUG ? 'http://localhost:8080' : 'https://cpduels-backend.onrender.com';
 console.log(backendOrigin);
 export default backendOrigin;
