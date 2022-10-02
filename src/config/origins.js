bcddfb21aa749c34cd65d69ca081d25f9c8528bd
JelBell1324 <jeffthegreat66@gmail.com>
2022-10-02 09:56:10 -0500

    cors

--- src/config/origins.js
+++ src/config/origins.js
@@ -1,4 +1,4 @@
 import DEBUG from "./debug";
-const backendOrigin = DEBUG ? 'http://localhost:8080' : 'https://hidden-oasis-47897.herokuapp.com/https://cpduels-backend.onrender.com';
+const backendOrigin = DEBUG ? 'http://localhost:8080' : 'https://hidden-oasis-47897.herokuapp.com/cpduels-backend.onrender.com';
 console.log(backendOrigin);
 export default backendOrigin;
