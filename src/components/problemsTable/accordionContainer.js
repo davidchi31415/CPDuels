fbb09437728087939a8184491f44767f477017a2
David Chi <davidchi@tamu.edu>
2022-10-12 22:18:33 -0500

    adjusted mathjax

--- src/components/problemsTable/accordionContainer.js
+++ src/components/problemsTable/accordionContainer.js
@@ -47,83 +47,82 @@ const AccordionContainer = ({ id, duelStatus, playerNum }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    problems?.length ?
-    <Accordion onChange={(index) => setSelectedProblem(index+1)} allowToggle
-      boxShadow='2xl'
-    >
-      {
-        problems.map(
-          (problem, index) => 
-          <AccordionItem key={problem._id} borderColor={borderColor} border='1px solid'>
-            <h2>
-              <AccordionButton bg={(index === selectedProblem-1) ? selectedRowColor : ''}
-                _hover={(index === selectedProblem-1) ? {bg: selectedRowColor} : {}}
-              >
-                <Box flex='2' textAlign='left'>
-                  {index+1}. <b>{problem.name}</b>
+    <MathJax>
+      { problems?.length ?
+      <Accordion onChange={(index) => setSelectedProblem(index+1)} allowToggle
+        boxShadow='2xl'
+      >
+        {
+          problems.map(
+            (problem, index) => 
+            <AccordionItem key={problem._id} borderColor={borderColor} border='1px solid'>
+              <h2>
+                <AccordionButton bg={(index === selectedProblem-1) ? selectedRowColor : ''}
+                  _hover={(index === selectedProblem-1) ? {bg: selectedRowColor} : {}}
+                >
+                  <Box flex='2' textAlign='left'>
+                    {index+1}. <b>{problem.name}</b>
+                  </Box>
+                  <Box flex='1' textAlign='center'>
+                    <b>Rated:</b> {problem.rating}, <b>Points:</b> {problem.points}
+                  </Box>
+                  <AccordionIcon/>
+                </AccordionButton>
+              </h2>
+              <AccordionPanel p={4}>
+                {problem.content.problemConstraints ? <Flex justify='space-between'>{parse(problem.content.problemConstraints)}</Flex> : ""}
+                <Box mt={2} className='problem-statement'>
+                  <Text fontWeight='bold' fontSize='1.2rem'>Problem Statement</Text>
+                  {problem.content.problemStatement ? parse(problem.content.problemStatement) : ""}
                 </Box>
-                <Box flex='1' textAlign='center'>
-                  <b>Rated:</b> {problem.rating}, <b>Points:</b> {problem.points}
+                <Box mt={2} className='problem-input-specifications'>
+                  <Text fontWeight='bold' fontSize='1.2rem'>Input</Text>
+                  {problem.content.problemInput ? parse(problem.content.problemInput) : ""}
                 </Box>
-                <AccordionIcon/>
-              </AccordionButton>
-            </h2>
-            <AccordionPanel p={4}>
-              {
-                console.log(problem.content)
-              }
-              {problem.content.problemConstraints ? <Flex justify='space-between'>{parse(problem.content.problemConstraints)}</Flex> : ""}
-            <Box mt={2} className='problem-statement'>
-              <Text fontWeight='bold' fontSize='1.2rem'>Problem Statement</Text>
-              {problem.content.problemStatement ? <MathJax>{parse(problem.content.problemStatement)}</MathJax> : ""}
-            </Box>
-            <Box mt={2} className='problem-input-specifications'>
-              <Text fontWeight='bold' fontSize='1.2rem'>Input</Text>
-              {problem.content.problemInput ? <MathJax>{parse(problem.content.problemInput)}</MathJax> : ""}
-            </Box>
-            <Box mt={2} className='problem-output-specifications'>
-              <Text fontWeight='bold' fontSize='1.2rem'>Output</Text>
-              {problem.content.problemOutput ? <MathJax>{parse(problem.content.problemOutput)}</MathJax> : ""}
-            </Box>
-            <Box mt={2} className='problem-sample-test-cases'>
-              <Text fontWeight='bold' fontSize='1.2rem'>Sample Test Cases</Text>
-              {problem.content.samples ? parse(problem.content.samples) : ""}
-            </Box>
-            <Center pt={3}>
+                <Box mt={2} className='problem-output-specifications'>
+                  <Text fontWeight='bold' fontSize='1.2rem'>Output</Text>
+                  {problem.content.problemOutput ? parse(problem.content.problemOutput) : ""}
+                </Box>
+                <Box mt={2} className='problem-sample-test-cases'>
+                  <Text fontWeight='bold' fontSize='1.2rem'>Sample Test Cases</Text>
+                  {problem.content.samples ? parse(problem.content.samples) : ""}
+                </Box>
+              <Center pt={3}>
                 <Button onClick={onOpen} size="md" fontSize='lg'
                   variant='solid' colorScheme='primary'
                 >
                   Submit Your Answer
                 </Button>
               </Center>
-            </AccordionPanel>
-          </AccordionItem>
-        )
-      }
-      <Modal isOpen={isOpen} onClose={onClose} size='3xl'>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Submit Your Answer</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody width='750px'>
-            <SubmitCodeEditor isPopup={true} problemChosen={selectedProblem} numProblems={problems.length} />
-          </ModalBody>
-          <ModalFooter justifyContent='center'>
-            <ButtonGroup spacing={0}>
-              <Button colorScheme='primary' mr={3}>
-                Submit
-              </Button>
-              <Button variant='outline' colorScheme='primary'
-                onClick={onClose}
-              >
-                Close
-              </Button>
-            </ButtonGroup>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Accordion>
-    : <p>Problems will be generated when the duel starts.</p>
+              </AccordionPanel>
+            </AccordionItem>
+          )
+        }
+        <Modal isOpen={isOpen} onClose={onClose} size='3xl'>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Submit Your Answer</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody width='750px'>
+              <SubmitCodeEditor isPopup={true} problemChosen={selectedProblem} numProblems={problems.length} />
+            </ModalBody>
+            <ModalFooter justifyContent='center'>
+              <ButtonGroup spacing={0}>
+                <Button colorScheme='primary' mr={3}>
+                  Submit
+                </Button>
+                <Button variant='outline' colorScheme='primary'
+                  onClick={onClose}
+                >
+                  Close
+                </Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Accordion>
+      : <p>Problems will be generated when the duel starts.</p>}
+    </MathJax>
   );
 }
 
