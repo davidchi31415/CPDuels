459d443c9645f32eb9efd9b11e1bb2a8ee6c5e3b
David Chi <davidchi@tamu.edu>
2022-11-14 15:51:10 -0600

    Mobile version 4.

--- src/components/baseLayout/index.js
+++ src/components/baseLayout/index.js
@@ -10,43 +10,125 @@ import {
   IconButton,
   Link,
   useColorMode,
+  useMediaQuery,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  List,
+  ListItem,
 } from "@chakra-ui/react";
 import { WiDaySunny } from "react-icons/wi";
 import { IoMoon } from "react-icons/io5";
 import LightLogo from "../../images/CPDuels Logo Light - NEW.svg";
 import DarkLogo from "../../images/CPDuels Logo Dark - NEW.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const HamburgerMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <IconButton
+        icon={<GiHamburgerMenu size={45} />}
+        onClick={onOpen}
+        boxSize={["4rem"]}
+        size={["4rem"]}
+        mr={0}
+      />
+      <Drawer placement="right" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Menu</DrawerHeader>
+          <DrawerBody>
+            <List fontSize="1.4rem" spacing={2}>
+              <ListItem>
+                <Link as={ReactLink} _hover={{ textDecoration: "none" }} to="/">
+                  Home
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  as={ReactLink}
+                  _hover={{ textDecoration: "none" }}
+                  to="/play"
+                >
+                  Play
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Link
+                  as={ReactLink}
+                  _hover={{ textDecoration: "none" }}
+                  to="/contact"
+                >
+                  Contact Us
+                </Link>
+              </ListItem>
+            </List>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
 
 const BaseNavbar = () => {
   const navigate = useNavigate();
   const { colorMode, toggleColorMode } = useColorMode();
   const navigateHome = () => navigate("/");
+  const [isMobile] = useMediaQuery("(max-width: 480px)");
 
-  return (
-    <Flex mt="1" justify="space-between">
-      <Image
-        aria-label="CPDuels logo"
-        src={colorMode === "light" ? LightLogo : DarkLogo}
-        w="10em"
-        h="auto"
-        cursor="pointer"
-        onClick={navigateHome}
-      />
-      <Spacer />
-      <HStack
-        fontSize="1.5rem"
-        fontWeight="bold"
-        spacing="1.5em"
-        width="fit-content"
+  if (!isMobile) {
+    return (
+      <Flex width="100%" mt="1" justify="space-between" px={2} pt={2}>
+        <Image
+          aria-label="CPDuels logo"
+          src={colorMode === "light" ? LightLogo : DarkLogo}
+          w="10em"
+          h="auto"
+          cursor="pointer"
+          onClick={navigateHome}
+        />
+        <HStack
+          fontSize="1.5rem"
+          fontWeight="bold"
+          spacing="1.5em"
+          width="fit-content"
+        >
+          <Link as={ReactLink} _hover={{ textDecoration: "none" }} to="/play">
+            Play
+          </Link>
+          <Link
+            as={ReactLink}
+            _hover={{ textDecoration: "none" }}
+            to="/contact"
+          >
+            Contact Us
+          </Link>
+        </HStack>
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex
+        mt="1"
+        justify="space-between"
       >
-        <Link as={ReactLink} _hover={{ textDecoration: "none" }} to="/play">
-          Play
-        </Link>
-        <Link as={ReactLink} _hover={{ textDecoration: "none" }} to="/contact">
-          Contact Us
-        </Link>
-      </HStack>
-    </Flex>
-  );
+        <Image
+          aria-label="CPDuels logo"
+          src={colorMode === "light" ? LightLogo : DarkLogo}
+          w="10em"
+          h="auto"
+          cursor="pointer"
+          onClick={navigateHome}
+        />
+        <HamburgerMenu />
+      </Flex>
+    );
+  }
 };
 
 const BaseContainer = ({ content }) => {
@@ -57,8 +139,12 @@ const BaseFooter = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Box width={["320px", "500px", "800px", "1000px", "1150px"]} position="absolute" bottom={0}>
-      <Text fontSize="md" mb={5} align="center">
+    <Box
+      width={["320px", "500px", "800px", "1000px", "1150px"]}
+      position="absolute"
+      bottom={0}
+    >
+      <Text fontSize={["sm", "md"]} mb={4} align="center">
         Developed by{" "}
         <Text
           as="span"
