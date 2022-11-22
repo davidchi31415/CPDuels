399387ce6fa0be7c41ce58302d0b497d666d42bf
David Chi <davidchi@tamu.edu>
2022-11-22 16:50:00 -0600

    Made contact page even better

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -27,26 +27,14 @@ const ContactPage = () => {
         <Flex direction="column" align="center">
           <Text
             textStyle="display2"
-            fontSize={["2.8rem", "3rem", "4rem"]}
-            lineHeight={["3.6rem", "4.8rem"]}
+            fontSize={["2.4rem", "3rem", "4rem"]}
+            lineHeight={["3.2rem", "4.8rem"]}
             maxWidth='90vw'
+            mx='auto'
           >
             Meet the Team
           </Text>
-          <Text
-            textStyle="body2"
-            fontSize={["1.1rem", "1.6rem"]}
-            lineHeight={["1.6rem", "2.4rem"]}
-            maxWidth='90vw'
-            mx="auto"
-          >
-            CPDuels is a personal project we developed over the course of Fall
-            2022. It has taken the two of us countless hours to complete, so we
-            hope you enjoy using it as much as we enjoyed building it.
-            <br />
-            Now here's a bit of info about us.
-          </Text>
-          <Flex gap="5em" justify="center" maxWidth='90vw' flexWrap='wrap' pt="2.5em" pb="5em">
+          <Flex gap={["3em", "5em"]} justify="center" maxWidth='90vw' flexWrap='wrap' pt={["1.25em", "2.5em"]} pb="5em">
             <ContactCard info={infoDavid} />
             <ContactCard info={infoJeffrey} />
           </Flex>
