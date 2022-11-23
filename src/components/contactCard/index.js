98db15de81d3e36326e395c9bbb630d12b1a95b4
David Chi <davidchi@tamu.edu>
2022-11-22 20:22:54 -0600

    Added mobile version of hover

--- src/components/contactCard/index.js
+++ src/components/contactCard/index.js
@@ -1,8 +1,16 @@
-import { Box, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  useColorMode,
+  useColorModeValue,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import React from "react";
-import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from "react-social-icons";
 
-const ContactCard = ({ info }) => {
+const ContactCard = ({ inViewport, forwardedRef, info }) => {
+  const {colorMode, toggle} = useColorMode();
   const border = useColorModeValue(
     "solid 1px rgb(0, 0, 0, 0.5)",
     "solid 1px #7d7dff"
@@ -12,40 +20,65 @@ const ContactCard = ({ info }) => {
     { boxShadow: "0 8px 50px" },
     { borderColor: "#ffd543", boxShadow: "#ffd543 0 8px 50px" }
   );
-  const githubColor = useColorModeValue('', '#f3f3f3');
+  const githubColor = useColorModeValue("", "#f3f3f3");
+  const [isMobile] = useMediaQuery("(max-width: 991px)");
 
   return (
-    <Box
-      p={[3, 5]}
-      rounded="md"
-      border={border}
-      boxShadow={shadow}
-      width={["300px", "360px"]}
-      height={["525px"]}
-      _hover={onHover}
-      transition="border-color 0.5s ease, box-shadow 0.5s ease"
-    >
+    <div ref={forwardedRef}>
       <Box
-        borderRadius="100%"
-        width="150px"
-        height="150px"
-        margin="0 auto"
-        border="solid 1px"
-      ></Box>
-      <Text margin="5px auto 0" width="fit-content" textStyle="body2Semi">
-        {info.name}
-      </Text>
-      <Text margin="0 auto" width="fit-content" fontSize="1.2rem">
-        {info.role}
-      </Text>
-      <Text margin="5px auto" mb={["35px", "15px"]} width="100%" height="150px" fontSize={["0.9rem", "1rem"]}>
-        {info.description}
-      </Text>
-      <Flex justify="center" gap={2}>
-        <SocialIcon url={info.linkedin} />
-        <SocialIcon url={info.github} bgColor={githubColor} />
-      </Flex>
-    </Box>
+        p={[3, 5]}
+        rounded="md"
+        border={
+          isMobile
+            ? inViewport
+              ? colorMode === "light"
+                ? "solid 1px rgb(0, 0, 0, 0.5)"
+                : "solid 1px #ffd543"
+              : border
+            : border
+        }
+        boxShadow={
+          isMobile
+            ? inViewport
+              ? colorMode === "light"
+                ? "0 8px 50px"
+                : "#ffd543 0 8px 50px"
+              : shadow
+            : shadow
+        }
+        width={["300px", "360px"]}
+        height={["525px"]}
+        _hover={isMobile ? "" : onHover}
+        transition="border-color 0.5s ease, box-shadow 0.5s ease"
+      >
+        <Box
+          borderRadius="100%"
+          width="150px"
+          height="150px"
+          margin="0 auto"
+          border="solid 1px"
+        ></Box>
+        <Text margin="5px auto 0" width="fit-content" textStyle="body2Semi">
+          {info.name}
+        </Text>
+        <Text margin="0 auto" width="fit-content" fontSize="1.2rem">
+          {info.role}
+        </Text>
+        <Text
+          margin="5px auto"
+          mb={["35px", "15px"]}
+          width="100%"
+          height="150px"
+          fontSize={["0.9rem", "1rem"]}
+        >
+          {info.description}
+        </Text>
+        <Flex justify="center" gap={2} py={0}>
+          <SocialIcon url={info.linkedin} />
+          <SocialIcon url={info.github} bgColor={githubColor} />
+        </Flex>
+      </Box>
+    </div>
   );
 };
 
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,24 +2,29 @@ import React from "react";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import BaseLayout from "../components/baseLayout";
 import ContactCard from "../components/contactCard";
+import handleViewport from "react-in-viewport";
 
 const ContactPage = () => {
   const infoDavid = {
     name: "David Chi",
     role: "Frontend",
-    description: 
-    `I am a junior Computer Science student at Texas A&M. I am primarily focused on web development and machine learning,
+    description: `I am a junior Computer Science student at Texas A&M. I am primarily focused on web development and machine learning,
     and I am seeking a software engineering internship where I can put my skills to use professionally.`,
-    linkedin: 'https://www.linkedin.com/in/david-chi-41392521a/',
-    github: 'https://github.com/davidchi31415',
+    linkedin: "https://www.linkedin.com/in/david-chi-41392521a/",
+    github: "https://github.com/davidchi31415",
   };
   const infoJeffrey = {
     name: "Jeffrey Li",
     role: "Backend",
-    description: "",
-    linkedin: 'https://www.linkedin.com/in/jeffrey-li-7b64b3221/',
-    github: 'https://github.com/JelBell1324',
+    description: `I am currently a junior at Texas A&M majoring in Computer Science. I am primarily focused on cybersecurity and am looking
+    for cybersecurity internships and opportunities.`,
+    linkedin: "https://www.linkedin.com/in/jeffrey-li-7b64b3221/",
+    github: "https://github.com/JelBell1324",
   };
+
+  const AnimatedContactCard = handleViewport(ContactCard, {
+    threshold: 0.75,
+  });
 
   return (
     <BaseLayout
@@ -29,14 +34,21 @@ const ContactPage = () => {
             textStyle="display2"
             fontSize={["2.4rem", "3rem", "4rem"]}
             lineHeight={["3.2rem", "4.8rem"]}
-            maxWidth='90vw'
-            mx='auto'
+            maxWidth="90vw"
+            mx="auto"
           >
             Meet the Team
           </Text>
-          <Flex gap={["3em", "5em"]} justify="center" maxWidth='90vw' flexWrap='wrap' pt={["1.25em", "2.5em"]} pb="5em">
-            <ContactCard info={infoDavid} />
-            <ContactCard info={infoJeffrey} />
+          <Flex
+            gap={["3em", "5em"]}
+            justify="center"
+            maxWidth="90vw"
+            flexWrap="wrap"
+            pt={["1.25em", "2.5em"]}
+            pb="5em"
+          >
+            <AnimatedContactCard info={infoDavid} />
+            <AnimatedContactCard info={infoJeffrey} />
           </Flex>
           <Text
             textStyle="display2"
